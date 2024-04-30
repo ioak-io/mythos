a606e4d7277eda93c761b21d88fe59385713f66c
manojvictor <fmanojvictor@gmail.com>
2024-04-30 15:10:41 +0530

    testcase page right and left listing changes

--- src/app/project/usecase/testcase/list/page.tsx
+++ src/app/project/usecase/testcase/list/page.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
 
 const testcases = () => {
   return (
     <div className="page">
-      <h1>Testcases/list Page</h1>
-      <p>This is the testcases/list page content.</p>
+      <div className="main-wrapper">
+        <div className="left">
+          <h1>Usecase Page</h1>
+        </div>
+        <div className="right">
+          <h1>Testcases/list Page</h1>
+          <p>This is the testcases/list page content.</p>
+        </div>
+      </div>
     </div>
   );
 };
