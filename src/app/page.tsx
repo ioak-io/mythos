8922ba641eee79399e501710130d76d8f8c68151
Mohana krishnan <mohana.krishnan@westernacher.com>
2024-05-29 12:40:52 +0530

    login page on load added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import LoginPage from "../app/login/page"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Main page
+      <LoginPage></LoginPage>
     </main>
   );
 }
